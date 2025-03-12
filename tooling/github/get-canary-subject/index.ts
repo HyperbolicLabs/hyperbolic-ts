2b35abad7f1bf6543c430bef687538e9c3001c40
Connor Chevli <connorchev@gmail.com>
2025-03-12 00:09:48 -0400

    add github actions for release flow (#2)
    * add github actions for release flow
    
    * fix deps
    
    * fix script location
    
    * update script path
    
    * bump ci
    
    * conditional release
    
    * fix composite action path
    
    * update script path
    
    * fix path
    
    * fix path
    
    * update changeset script
    
    * try adding github token
    
    * update repo link
    
    * private github package
    
    * adjust package version
    
    * maybe fix npm permissions
    
    * add github token
    
    * remove token input to github canary branch name script
    
    * logging
    
    * fix changeset command
    
    * verify canary subject
    
    * fix composite action output
    
    * try to fix script
    
    * fix output
    
    * fix output
    
    * fix
    
    * Revert
    
    * wip
    
    * Revert "wip"
    
    This reverts commit 38c35899de16b773244e575b5ea1cb4e992453cf.
    
    * simplify canary subject
    
    * update path
    
    * try fixing path
    
    * try a different approach
    
    * debug
    
    * fix path
    
    * remove extraneous logs
    
    * remove debug
    
    * final setup

---
+++ tooling/github/get-canary-subject/index.ts
@@ -0,0 +1,29 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import * as colors from "colorette";
+
+export function run(): string {
+  try {
+    // Remove the `refs/heads/` prefix from the branch name
+    const rawBranchName = github.context.ref.split("refs/heads/").pop();
+
+    // If the branch name is undefined, throw an error
+    if (!rawBranchName) {
+      throw new Error("Unable to detect branch name.");
+    }
+
+    // Truncate the name to a reasonable length and
+    // replace all non-alphanumeric characters with a dash
+    const sanitizedBranchName = rawBranchName.slice(0, 30).replace(/[^a-zA-Z0-9]/g, "-");
+
+    return sanitizedBranchName;
+  } catch (error) {
+    console.error(colors.redBright("Error getting canary subject:"));
+    console.error(colors.redBright((error as { message: string }).message));
+    core.setFailed((error as { message: string }).message);
+    process.exit(1);
+  }
+}
+
+// Print the result to stdout so it can be captured by the bash command
+console.log(run());
