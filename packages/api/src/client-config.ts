ad6d5607318f57800cfec9c694c6ff05b2b20f2f
magqqgq <146786427+magqqgq@users.noreply.github.com>
2026-09-01 01:48:46 +0300

    Update client-config.ts

--- packages/api/src/client-config.ts
+++ packages/api/src/client-config.ts
@@ -5,7 +5,7 @@ export const HYPERBOLIC_API_BASE_URL = "https://api.hyperbolic.xyz";
 
 export const createClientConfig: CreateClientConfig = (config) => {
   return {
-    // Place default properties first so that any custom properties or overrides
+    // Set fallback options first so that any custom properties or overrides
     // provided by the caller via `config` take correct precedence.
     baseUrl: config?.baseUrl ?? HYPERBOLIC_API_BASE_URL,
     auth: config?.auth ?? process.env.HYPERBOLIC_API_KEY,
